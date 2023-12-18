09123f6c9d788d1fd08d2f7f2423410bf909755d
Masud Al Imran <masudalimran92@gmail.com>
2023-12-18 11:30:47 +0600

    feat:migration in database

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,43 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-{{ app()->getLocale() }}
+<html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>My Laravel App</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Demo 1</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="antialiased">
-    <p class="">Hello World</p>
+<body>
+    <main>
+        <nav class="flex justify-end items-center gap-4 pr-4 py-4 shadow-xl sticky top-0 bg-white z-10">
+            <a href="{{ route('login') }}" class="font-semibold hover:opacity-50">
+                Login
+            </a>
+            <a href="{{ route('register') }}" class="font-semibold hover:opacity-50">
+                Register
+            </a>
+        </nav>
+        <div class="px-8 py-4">
+            <p class="text-2xl text-center underline">Home Page</p>
+            <p class="text-sm text-justify w-[50%] m-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Veniam
+                excepturi
+                deleniti, quos ut placeat maiores
+                harum non perferendis eum ducimus laudantium at, praesentium eligendi aliquam doloremque repellendus
+                labore
+                architecto nisi?</p>
+        </div>
+        <div class="flex flex-wrap justify-center items-center gap-4 mt-4">
+
+            @for ($i = 0; $i < 10; $i++)
+                <img src="https://source.unsplash.com/random/800x800/?img={{ $i + 1 }}"
+                    class="cover h-[800px] rounded opacity-100 hover:opacity-50 cursor-pointer transition-opacity duration-150 delay-75" />
+            @endfor
+
+        </div>
+    </main>
 </body>
 
 </html>
